37a1dc9f3213a8edbefef6cdf993cc5f252b37e8
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-01 10:38:21 +0530

    `DARKSPMCHAT`

--- handlers/start.py
+++ handlers/start.py
@@ -1,62 +1,51 @@
-from database.db import Database
-from pyrogram.errors.exceptions.bad_request_400 import *
-from pyrogram.errors import *
-from pyrogram import *
-from pyrogram.errors import *
-from pyrogram.types import *
+import os
+
+from pyrogram import Client, filters
+from presets import Presets
+
 from config import *
-import logging
-from database.db import Database
-from asyncio import sleep
-import traceback
-from helper.fsub import forcesub
 
-DATABASE_URL=MONGO_URI
-db = Database(DATABASE_URL, "DARKS_PM")     
-
-@Client.on_message(filters.command("start"))
-async def start(client, message):
-    if await forcesub(client, message):
-       return
-    #return
-    chat_id = message.from_user.id
-    if not await db.is_user_exist(chat_id):
-        data = await client.get_me()
-        BOT_USERNAME = data.username
-        await db.add_user(chat_id)
-        if LOG_CHANNEL:
-            await client.send_message(
-                LOG_CHANNEL,
-                f"#NEWUSER: \n\n**User:** [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n**ID:**{message.from_user.id}\n Started @{BOT_USERNAME} !!",
-            )
-        else:
-            logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
-    info = await bot.get_users(user_ids=OWNER_ID)
-    file_id = START_STICKER
-    await client.send_sticker(message.chat.id, file_id, reply_markup=start_menu)
-    text = START_STRING.format(message.from_user.mention, info.first_name)
-    reply_markup = START_BUTTON  
-    await message.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
+@Client.on_message(filters.private & filters.command(["start"]))
+async def force_sub(bot, msg):
+    if force_subchannel:
+        try:
+            user = await bot.get_chat_member(force_subchannel, msg.from_user.id)
+            if user.status == "kicked out":
+                await msg.reply_text("Yourt Banned")
+                return 
+        except UserNotParticipant:
+            await msg.reply_text text= 
+                text="**❌ Access Denied ❌**\n🌷You Must Join My Update Channel...🌷\n♻️Join it & Try Again.♻️",
+                reply_markup=InlineKeyboardMarkup([[
+                 InlineKeyboardButton('𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊', url='https://t.me/ImDark_Empire'),
+                 InlineKeyboardButton('𝕯𝖆𝖗𝖐 𝕰𝖒𝖕𝖎𝖗𝖊', url='https://t.me/SL_BOTS_TM')
+                 ]]
+                )
+            )    
+            return 
+    
+@Client.on_message(filters.command(commands=['start'])) 
+  async def StartMsg(_,m): 
+  await client.send_sticker(m.chat.id, sticker='CAACAgUAAxkBAAEBE5VidQNiLpHSvN1iicpZKZHdZ-lKvQACAwQAAidtOVUW_Kpa6C8wfSQE')
+    
+@Client.on_message(filters.private & filters.command(['start', 'help']))
+async def help_me(bot, message):
+    if message.from_user.id == Config.ADMIN:
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text=Presets.WELCOME_TEXT.format(info.first_name)
     )
-  
-
-@Client.on_message(filters.command("help"))
-async def help(bot, message):
-  await bot.send_sticker(message.chat.id, STAT_STICKER)
-  await message.reply_text(text=HELP_STRING,reply_markup=HELP_BTN)
-
-    
-    
-@Client.on_callback_query(filters.regex("stback"))
-async def start_menu(_,query):
-  await query.answer()
-  await query.message.edit(START_STRING,reply_markup=START_BUTTON)
-
-@Client.on_callback_query(filters.regex("hlp"))
-async def help_menu(_,query):
-  await query.answer()
-  await query.message.edit(HELP_STRING,reply_markup=HELP_BTN)
+    await bot.send_message(
+        chat_id=Config.ADMIN,
+        text=Presets.USER_DETAILS.format(
+            info.first_name,
+            info.last_name,
+            info.id, info.username,
+            info.is_scam,
+            info.is_restricted,
+            info.status,
+            info.dc_id
+        )
+    )
