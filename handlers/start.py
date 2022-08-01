dba899f8b8dda3f6ebe3d559a3ca6ed7a44f0840
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-01 13:04:52 +0530

    `DARKSPMCHAT`

--- handlers/start.py
+++ handlers/start.py
@@ -1,20 +1,16 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
-import random
 
-STICKERS = ["CAACAgUAAxkBAAIEI2Lh9wABwA5eCscl00ee7IQgUeJ8KAACHgUAAqrCAAFVByHxjhsfLyEeBA", "CAACAgUAAxkBAAIEIWLh9vxLot7QZlvMp1UT_QrX4O92AAJ5BgACNpEBVcaIt8a3oOWLHgQ"]
 
 # Start Message
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def start(anonbot, msg):
-    print("/start")
-    user = await anonbot.get_me()
-    mention = user["mention"]
-    STICKER = random.choice(STICKERS)
-    await anonbot.send_message(
-        msg.reply_sticker(STICKER),
-        msg.chat.id,
-        Data.START.format(msg.from_user.mention, mention),
-        reply_markup=InlineKeyboardMarkup(Data.buttons),
-    )
+async def start(bot, msg):
+	user = await bot.get_me()
+	mention = user["mention"]
+	await bot.send_sticker(msg.chat.id, "CAACAgUAAxkBAAIEIWLh9vxLot7QZlvMp1UT_QrX4O92AAJ5BgACNpEBVcaIt8a3oOWLHgQ")
+	await bot.send_message(
+		msg.chat.id,
+		Data.START.format(msg.from_user.mention, mention),
+		reply_markup=InlineKeyboardMarkup(Data.buttons),
+	)
