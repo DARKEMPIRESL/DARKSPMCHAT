020ee022a7f654867d6f4fd0b017cbb992a87618
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-02 08:02:53 +0530

    `DARKSPMCHAT`

--- handlers/database/users_sql.py
+++ handlers/database/users_sql.py
@@ -6,6 +6,7 @@ class Users(BASE):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
     user_id = Column(Integer, primary_key=True)
+    channels = Column(String, nullable=True)
 
     def __init__(self, user_id):
         self.user_id = user_id
