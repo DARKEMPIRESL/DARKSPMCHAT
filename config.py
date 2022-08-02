4a2a3d2a1f40666d65bdd6005da9fa3d2ca2a300
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-02 21:11:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # from @botfather
 API_ID = int(os.getenv("API_ID", ""))  # from https://my.telegram.org/apps
 API_HASH = os.getenv("API_HASH", "")  # from https://my.telegram.org/apps
-force_subchannel = os.getenv("FSUB", "SLBotOfficial")
+MUST_JOIN = os.getenv("MUST_JOIN", "SLBotOfficial")
 OWNER_ID = int(os.environ.get("OWNER_ID", "1120271521"))
 DATABASE_URL = os.environ.get('DATABASE_URL', None)
 DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
