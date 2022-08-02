d7d83fdf7f6c400631a41877379b9b45828a023e
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-02 19:17:51 +0530

    `DARKSPMCHAT`

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,4 +8,4 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-Client = Client("DARK Pm Bot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+DARK = Client("DARK Pm Bot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
