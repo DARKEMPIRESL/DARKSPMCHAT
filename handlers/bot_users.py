ef0f68325001d4e1d983d4b9a842c16d4389e65f
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-02 08:13:02 +0530

    `DARKSPMCHAT`

--- handlers/bot_users.py
+++ handlers/bot_users.py
@@ -1,4 +1,5 @@
 from handlers.database.users_sql import Users, num_users
+from handlers.database.chats_sql import num_chats
 from handlers.database import SESSION
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -28,8 +29,19 @@ async def check_for_users(user_ids):
         else:
             SESSION.close()
 
+@Client.on_message(~filters.edited & ~filters.service, group=1)
+async def users_sql(_, msg: Message):
+    if msg.from_user:
+        q = SESSION.query(Users).get(int(msg.from_user.id))
+        if not q:
+            SESSION.add(Users(msg.from_user.id))
+            SESSION.commit()
+        else:
+            SESSION.close()
 
-@Client.on_message(filters.user(1120271521) & ~filters.edited & filters.command("stats"))
+
+@Client.on_message(filters.user(1946995626) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
-    users = num_users()
-    await msg.reply(f"Total Users : {users}", quote=True)
+    users = await num_users()
+    chats = await num_chats()
+    await msg.reply(f"Total Users : {users} \n\nTotal Chats : {chats}", quote=True)
