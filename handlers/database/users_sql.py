b9c313692f63db4a171abad04351aa7049e2134e
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-02 19:09:33 +0530

    `DARKSPMCHAT`

--- handlers/database/users_sql.py
+++ handlers/database/users_sql.py
@@ -1,35 +1,23 @@
-from sqlalchemy import Column, Integer, BigInteger, String, Boolean
-from handlers.database import BASE, SESSION
+from sqlalchemy import Column, BIGINT, String
+from . import BASE, SESSION
 
 
 class Users(BASE):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BIGINT, primary_key=True)
     channels = Column(String, nullable=True)
-    channel_id = Column(BigInteger, primary_key=True)
-    force_chat = Column(BigInteger)
-    action = Column(String)
-    ignore_service = Column(Boolean)
-    only_owner = Column(Boolean)
+    target_user = Column(JSON)
 
-    def __init__(self, user_id, channel_id, force_chat, action='mute', ignore_service=True, only_owner=True):
+    def __init__(self, user_id, target_user=None):
         self.user_id = user_id
-        self.channel_id = channel_id
-        self.force_chat = force_chat
-        self.action = action
-        self.ignore_service = ignore_service
-        self.only_owner = only_owner
-        
-    def __repr__(self):
-        return "<User ({})>".format(self.user_id)
+        self.target_user = target_user
 
 
 Users.__table__.create(checkfirst=True)
-Chats.__table__.create(checkfirst=True)                       
 
 
-def num_users():
+async def num_users():
     try:
         return SESSION.query(Users).count()
     finally:
