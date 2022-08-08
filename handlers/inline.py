52d12f39079f328bba4ed5917187109dade321f1
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-08 19:53:25 +0530

    `DARKSPMCHAT`

--- handlers/inline.py
+++ handlers/inline.py
@@ -17,6 +17,7 @@ from handlers.database import SESSION
 from handlers.bot_users import check_for_users
 from Data import Data
 from handlers.calculator import *
+from handlers.callbacks import *
 
 
 main = [
@@ -205,3 +206,39 @@ async def answer(bot: Client, query):
                 switch_pm_parameter="start"
             )
     await check_for_users(sender)
+
+@Client.on_inline_query()
+async def inline(bot, update):
+    if len(update.data) == 0:
+        try:
+            answers = [
+                InlineQueryResultArticle(
+                    title="Calculator",
+                    description=f"New calculator",
+                    input_message_content=InputTextMessageContent(
+                        text=CALCULATE_TEXT,
+                        disable_web_page_preview=True
+                    ),
+                    reply_markup=CALCULATE_BUTTONS
+                )
+            ]
+        except Exception as error:
+            print(error)
+    else:
+        try:
+            message_text = update.message.text.split("\n")[0].strip().split("=")[0].strip()
+            data = message_text.replace("×", "*").replace("÷", "/")
+            text = float(eval(data))
+            answers = [
+                InlineQueryResultArticle(
+                    title="Answer",
+                    description=f"Results of your input",
+                    input_message_content=InputTextMessageContent(
+                        text=f"{data} = {text}",
+                        disable_web_page_preview=True
+                    )
+                )
+            ]
+        except:
+            pass
+    await update.answer(answers)
